c9ba7b68823a8fbb8d535c6dc7f17687b38a1c10
이유림 <ylee981352@gmail.com>
2021-07-19 11:49:18 +0900

    비트 연산자

--- JavaEx/src/com/javaex/basics/OperatorEx.java
+++ JavaEx/src/com/javaex/basics/OperatorEx.java
@@ -6,6 +6,31 @@ public class OperatorEx {
 	   logicOperEx();
 	}
 	
+	// 비트 연산자
+	private static void bitOperEx(){
+		// 하드웨어 제어, 이미지 처리등
+		// 미세하게 비트 단위 데이터 제어에 활용
+		byte b1 = 0b1101;
+		byte b2 = 0b0111;
+		
+		   System.out.println("b1:" + Integer.toBinaryString(b1));
+		   System.out.println("b2:"+ Integer.toBinaryString(b2));
+		   
+		   int result = b1 &b2; // 비트 논리곱
+		   System.out.println("b1 & b2 : " + Integer.toBinaryString(result));
+		   
+		   result =b1 | b2; // 비트 논리합
+		   System.out.println("b1 | b2 : " + Integer.toBinaryString(result));
+		   
+		   result = ~b1; // 비트 논리 부정
+		   System.out.println("~b1 : " +  Integer.toBinaryString(result));
+		   
+		   result = b1 ^ b2; //베타적 논리합 
+		   System.out.println("b1 ^b2 : " + Integer.toBinaryString(result));
+		   
+		     
+	}
+	
 	// 비교 연산과 논리연산
 	private static void logicOperEx() {
 	 int a =7, b=3;
