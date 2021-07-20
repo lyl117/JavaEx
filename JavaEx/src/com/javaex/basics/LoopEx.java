e463624bce04628244920feb46ec9d1931036209
이유림 <ylee981352@gmail.com>
2021-07-20 17:20:30 +0900

    반복문

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -7,9 +7,64 @@ public class LoopEx {
 	public static void main(String[] args) {
 //		whileEx();
 //		whileGugu();
-		dowhileEx();
+//		dowhileEx();
+//		forGugu();
+//		continueEx();
+//		forGugu2();
+		forPractice(); 
 		System.out.println("End of Code");
 
+	}
+	private static void forPractice () {
+		for(int i=1; i<=6; i++) {
+			for(int j=1; j<i; j++) {
+				System.out.println("*");
+				if (j==i) {
+					System.out.println();
+				}
+			}
+		}
+	}
+	private static void forGugu2() {
+		Scanner scanner = new Scanner(System.in);
+		
+		int j = scanner.nextInt();
+		
+		for(int i =0; i<=9; i++) {
+			(int j= 2; j<=10; j++){
+			}
+			
+			System.out.println(j +"*"+i+"="+(i*j));
+			}
+		
+		scanner.close();
+		}
+
+			
+		
+	
+	private static void continueEx() {
+		for(int i = 1; 
+				i <= 20 ;
+				i ++) {
+			// 2의 배수, 3의 배수는 출력하지 않음
+			if(i % 2 == 0||i %3 ==0)
+				continue;
+			System.out.println(i);
+		}
+	}
+	private static void forGugu() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("단을 입력하세요");
+		int dan = scanner.nextInt();
+		
+		for(int i = 0; //초기화
+	        i <= 9;   //반복 조건
+			i++) {   // 증감식
+			System.out.println(dan+"*"+i +"="+(dan*i));
+		}
+		scanner.close();
 	}
 	private static void dowhileEx() {
 		// 적어도 한번은 수행되어야 하는 반복,
