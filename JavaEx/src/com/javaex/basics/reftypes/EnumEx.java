43126c3d1bb33f90113a96e339127eec7c9979be
mac <mac@218.38.137.28>
2021-07-21 11:49:59 +0900

    enum 타입과 선언

---
+++ JavaEx/src/com/javaex/basics/reftypes/EnumEx.java
@@ -0,0 +1,22 @@
+package com.javaex.basics.reftypes;
+
+public class EnumEx {
+
+	public static void main(String[] args) {
+	  enumEx();
+	}
+  
+	public static void enumEx () {
+		Week today = Week.WEDNESDAY;
+		
+		System.out.printf(
+				"오늘은 %s (%d) 입니다."
+			today.name(), // 열거 상수 -> 문자열 
+		    today.ordinal ()) ; // 열거 상수 내순번 
+		
+		Week obj = Week.valueOf("FRIDAY");
+		
+		System.out.printf("%s , %d",
+				obj.name(), obj.ordinal());
+	}
+}
